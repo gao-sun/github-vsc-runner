413a5b4ada9bbf359b8b32a541506f7f7f957259
Gao Sun <gaosun.dev@gmail.com>
2021-03-07 19:54:08 +0800

    Remove macOS 11

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -49,7 +49,6 @@ export type RunnerClientHttpResponse = {
 export enum RunnerClientOS {
   Ubuntu_20_04 = 'ubuntu-20.04',
   Ubuntu_18_04 = 'ubuntu-18.04',
-  macOS_11_0 = 'macos-11.0',
   macOS_10_15 = 'macos-10.15',
   Windows_2019 = 'windows-2019',
 }
